df64a18a1d871cd50b6d272c6d9bf2f37150ee2e
Mathis <120995618+shell-don@users.noreply.github.com>
2024-09-05 18:08:30 +0200

    Update César.c

--- src/César.c
+++ src/César.c
@@ -1,74 +1,93 @@
 #include <stdio.h>
-#include <wchar.h>    
-#include <locale.h>   
+#include <stdlib.h>
+#include <wctype.h>
+#include <wchar.h>
+#include <locale.h>
 
-#define MAX_UNICODE 0x3134F          
 #define DECALAGE 2
-#define MAX_MESSAGE_LENGTH 100
 
-typedef struct { wchar_t caracter ; wchar_t cipher_caracter ; } Mapping ;
+int main(void) {
+	
+	setlocale(LC_ALL, "") ;
 
+	size_t buffer_size = 1024 ;
+	char Choice[3] ;
 
-int main () {
+	printf("Voulez-vous chiffrer (1) ou déchiffrer (2) ?\n") ;
+	fgets(Choice, 3, stdin) ;
 
-   setlocale(LC_ALL, "") ;
+	if (*(&Choice[0]) != '1' && *(&Choice[0]) != '2') {
+        	printf("Choix invalide.\n");
+        	return 1 ; 
+    	}	
 
-   wchar_t Caracter[MAX_UNICODE], Cipher_Caracter[MAX_UNICODE], Message[MAX_MESSAGE_LENGTH] ;
-   Mapping Alphabet[MAX_UNICODE] ;
-   int Choice ;
-   
-   printf("Voulez-vous Chiffrer (1) ou Déchiffrer (2) ?\n") ;
-   scanf("%d", &Choice) ;
-   // Vider le tampon d'entrée après scanf
-   getchar();
+	wchar_t* buffer = (wchar_t*)(calloc(buffer_size, sizeof(wchar_t))) ;
+	if (buffer == NULL) {
+        	fprintf(stderr, "Erreur d'allocation de mémoire pour le tampon\n") ;
+        	return 1 ; 
+	}
+        
+	fprintf(stdout, "Entrez le message : \n") ;
 
-   if (Choice == 1) {
+        if (fgetws(buffer, buffer_size, stdin) == NULL) {
+        	fprintf(stderr, "Erreur de lecture de l'entrée\n") ;
+        	free(buffer) ;  // Libérer le tampon alloué dynamiquement
+        	return 1 ;
+	}
 
-      // établissement des deux tableau et leurs lien
-      for (int i = 0 ; i < MAX_UNICODE ; i++) {
-         wchar_t wc = i ;                           
-         Caracter[i] = wc ; Cipher_Caracter[i] = wc+DECALAGE ;    
-         Alphabet[i].caracter = Caracter[i] ;
-         Alphabet[i].cipher_caracter = Cipher_Caracter[i] ; 
-      } 
+	size_t size_message = wcslen(buffer) ; //ne compte pas le \0 de fin
+	wchar_t* message = (wchar_t*)(calloc(size_message + 1, sizeof(wchar_t))) ;
+	if (buffer == NULL) {
+        	fprintf(stderr, "Erreur d'allocation de mémoire pour le tampon du message\n") ;
+        	free(buffer) ;
+		return 1 ; 
+	}
 
-      printf("Entrez le Message à Chiffrer : \n") ;
-      fgetws(Message, sizeof(Message), stdin) ;                   
-   
-      // modification du Message par la valeur décalé
-      for (int i = 0; Message[i] != '\0' && i < MAX_MESSAGE_LENGTH; i++) {
-         for (int j = 0; j < MAX_UNICODE; j++) {
-            if (Message[i] == Caracter[j]) {Message[i] = Alphabet[j].cipher_caracter ; break ;}
-         }
-         wprintf(L"%lc", Message[i]) ;
-      }
-   return 0 ;
-   }              
+	wcscpy(message, buffer) ;
+	free(buffer) ;
 
-   if (Choice == 2) { 
+	wchar_t* cipher_message = (wchar_t*)(calloc(size_message + 1, sizeof(wchar_t))) ; 
+	if (cipher_message == NULL) {
+		fprintf(stderr, "Erreur d'allocation de mémoir pour le tampon cipher message\n") ;
+		free(message) ;
+		return 1 ;
+	}
 
-      // établissement des deux tableau et leurs lien
-      for (int i = 0 ; i < MAX_UNICODE ; i++) {
-         wchar_t wc = i ;                           
-         Caracter[i] = wc ; Cipher_Caracter[i] = wc-DECALAGE ;    
-         Alphabet[i].caracter = Caracter[i] ;
-         Alphabet[i].cipher_caracter = Cipher_Caracter[i] ; 
-      } 
+	if (*(&Choice[0]) == '1') {
 
-      printf("Entrez le Message à Déchiffrer : \n") ;
-      fgetws(Message, sizeof(Message), stdin) ;                    
-   
-      // déchiffrement
-      for (int i = 0; Message[i] != '\0' && i < MAX_MESSAGE_LENGTH; i++) {
-         for (int j = 0; j < MAX_UNICODE; j++) {
-            if (Message[i] == Caracter[j]) {Message[i] = Alphabet[j].cipher_caracter ; break ;}
-         }
-         wprintf(L"%lc", Message[i]) ;
-      }
-   return 0 ;
-   }
-return 1 ;    
-} 
+	// Chiffrement
+	for (int i = 0 ; i < size_message ; i++) {
+		cipher_message[i] = (wchar_t)((((unsigned long long)message[i]) + DECALAGE)%0x10FFFF) ;
+	} 
+	
+	// Affichage
+	fwprintf(stdout, L"%ls", cipher_message) ;
+
+	free(message) ;
+	free(cipher_message) ;
+	
+	return 0 ;
+	}
+
+	if (*(&Choice[0]) == '2') {	
+
+	// Déhiffrement
+	for (int i = 0 ; i < size_message ; i++) {
+		cipher_message[i] = (wchar_t)((((unsigned long long)message[i]) - DECALAGE)%0x10FFFF) ;
+	} 
+	
+	// Affichage
+	fwprintf(stdout, L"%ls", cipher_message) ;
+
+	free(message) ;
+	free(cipher_message) ;
+	
+	return 0 ;
+	}
+
+	return 1 ;
+
+}
 
 // Contact : 
 // rkicuuqwocvjkuBiockn0eqo 
